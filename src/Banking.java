a84d8e375792c31f66e7a87234a0594d2fd4dce4
Amber Gardner <ambergardner@Emilys-MacBook-Air.local>
2017-04-06 11:49:44 -0500

    corrections

--- src/Banking.java
+++ src/Banking.java
@@ -1,27 +1,31 @@
-import javax.xml.bind.SchemaOutputResolver;
 import java.util.Scanner;
+import java.util.HashMap;
 
-/**
- * Created by amber on 2/3/17.
- */
 public class Banking {
 
     public static void main(String[] args) throws Exception {
 
+        HashMap<String, Double> hmap = new HashMap<String, Double>();
+
+        hmap.put("Amber", new Double(1000000.82));
+        hmap.put("Chase", new Double(500000.06));
+        hmap.put("AndrewCaleb", new Double(500000.15));
+
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What is your name?");
-
+        System.out.println("Please enter is your name");
         String name = scanner.nextLine();
 
-        if (name.isEmpty()) {
-            throw new Exception("name not entered");
-        } else {
+        if (hmap.containsKey(name)) {
+
             System.out.println("Welcome, " + name);
+
+        } else {
+            throw new Exception("Name not recognized. Would you like to create an account?");
         }
 
-        System.out.println("How can I help you today? Please select from the following: [1.checkBalance 2.WithdrawFunds 3.Cancel]");
+        System.out.println("How can I help you today? Please select from the following: [1.checkBalance 2.WithdrawFunds 3.Delete Account 4.Cancel]");
 
         int response = scanner.nextInt();
 
@@ -42,11 +46,23 @@ public class Banking {
                 break;
 
             case 3:
+                System.out.println("Are you sure you want to delete this account? [y/n]");
+                String answer = scanner.nextLine();
+                if (answer.equalsIgnoreCase("y")) {
+                    hmap.remove(hmap);
+                    System.out.println("Your account has been deleted.");
+                }
+                break;
+
+            case 4:
                 System.out.println("Thank you and please come again");
                 break;
 
             default:
                 System.out.println("Invalid option entered!");
+                }
+
         }
+
     }
-}
+
